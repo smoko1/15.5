ab8a683a4f57b8e17a3b52e5d2d87acc45d175d1
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@vp.pl>
2019-05-12 19:49:10 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function Button(text) {
+    this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    constructor: Button,
+    create: function () {
+        const self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        this.$element.click(function () {
+            alert(self.text);
+        });
+        this.$element.appendTo($('body'));
+    }
+};
+
+const btn1 = new Button('Hello!');
+btn1.create();
