dde82380c417d885ed84b37c8f251f6b36826068
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@vp.pl>
2019-05-24 21:55:34 +0200

    Change script file

--- js/script.js
+++ js/script.js
@@ -3,19 +3,15 @@
 function Button(text) {
     this.text = text || 'Hello';
 }
-
 Button.prototype = {
-    constructor: Button,
-    create: function () {
-        const self = this;
-        this.$element = $('<button>');
-        this.$element.text(this.text);
-        this.$element.click(function () {
+    create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
             alert(self.text);
         });
-        this.$element.appendTo($('body'));
-    }
-};
-
-const btn1 = new Button('Hello!');
+        document.body.appendChild(this.element);
+    }};
+var btn1 = new Button('Hello!');
 btn1.create();
